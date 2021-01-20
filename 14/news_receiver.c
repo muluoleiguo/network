7a142e95befec98b8cbc04f3174b9e62320147a1
muluo <xdw550184692@gmail.com>
2021-01-20 17:01:25 +0800

    14

---
+++ 14/news_receiver.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<signal.h>
+#include<netinet/in.h>
+#include<fcntl.h>
+
+
+#define BUF_SIZE 30
+void error_handling(char* message);
+
+int recv_sock;
+
+int main(int argc,char* argv[])
+{
+    struct sockaddr_in adr;
+    int str_len;
+    struct ip_mreq join_adr;
+    char buf[BUF_SIZE];
+    if(argc!=2)
+    {
+        printf("Usage : %s <port>\n",argv[0]);
+        exit(1);
+    }
+
+
+    recv_sock =socket(PF_INET,SOCK_DGRAM,0);
+    if(recv_sock == -1)
+        error_handling("socket() error");
+
+    memset(&adr,0,sizeof(adr));
+    adr.sin_family = AF_INET;
+    adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    adr.sin_port = htons(atoi(argv[1]));
+
+    if(bind(recv_sock,(struct sockaddr*)&adr,sizeof(adr))==-1)
+        error_handling("bind() error");
+
+    join_adr.imr_multiaddr.s_addr=inet_addr(argv[1]);
+    join_adr.imr_interface.s_addr=htonl(INADDR_ANY);
+    setsockopt(recv_sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (void*) &join_adr,sizeof(join_adr));
+
+    while(1)
+    {
+        str_len=recvfrom(recv_sock,buf,BUF_SIZE-1,0,NULL,0);
+        if(str_len<0)
+            break;
+        buf[str_len]=0;
+        fputs(buf,stdout);
+    }
+    close(recv_sock);
+    return 0;
+}
+
+
+
+void error_handling(char* message)
+{
+    fputs(message,stderr);
+    fputc('\n',stderr);
+    exit(1);
+}
+
